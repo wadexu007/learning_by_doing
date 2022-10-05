0e92a8555abc4f977cfba63cb4e2095f0178c9ef
wadexu007 <48537883+wadexu007@users.noreply.github.com>
2022-10-05 17:34:59 +0800

    test auto plan

--- Terraform/demo/README.md
+++ Terraform/demo/README.md
@@ -10,4 +10,3 @@ terraform plan
 
 terraform apply
 ```
-
--- Terraform/demo/network.tf
+++ Terraform/demo/network.tf
@@ -1,6 +1,6 @@
 resource "google_compute_network" "default" {
   project                 = local.project.project_id
   name                    = local.project.network_name
-  auto_create_subnetworks = false
+  auto_create_subnetworks = true
   routing_mode            = "GLOBAL"
 }

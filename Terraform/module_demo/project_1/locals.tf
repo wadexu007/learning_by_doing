29c45d48e56eb5fa4694e0a754abc35a86f715d9
Wade Xu <wade.xu@project44.com>
2023-10-20 16:38:26 +0800

    Atlantis custom workflow

--- Terraform/module_demo/project_1/locals.tf
+++ Terraform/module_demo/project_1/locals.tf
@@ -1,7 +1,7 @@
 locals {
   # project details
   project = {
-    project_id   = "adept-presence-396401"
+    project_id  = "adept-presence-396401"
     region       = "asia-east2"
     network_name = "default"
   }
